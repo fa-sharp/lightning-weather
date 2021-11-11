11f2df478e29fc3f62a4f8fd3a5ecf45b726abcb
fa-sharp <farshadpiano@gmail.com>
2021-11-11 03:05:42 -0600

    🗄️ Added lookupNames route - Returns only city names rather than fully city data

--- cities_api/server/index.js
+++ cities_api/server/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { normalizeQuery, requireAPIKey, validLimit } from './utils.js';
-import searchCitiesDB from './searchCitiesDB.js';
+import searchCitiesDB, { lookupNamesDB } from './searchCitiesDB.js';
 
 export const API_KEY = process.env.API_KEY || null; // API Key to secure the server (sort of)
 const PORT = process.env.PORT || 3001;
@@ -22,6 +22,25 @@ app.get("/lookup", requireAPIKey, (req, res) => {
 
     try {
         const results = searchCitiesDB(normalizedQuery, limit);
+        return res.json(results);
+    }
+    catch (err) {
+        console.error("Error searching for cities in database: ", err);
+        return res.status(500).send({ reason: "Error searching for cities :(" })
+    }
+})
+
+app.get("/lookupNames", requireAPIKey, (req, res) => {
+    const { search, top } = req.query;
+
+    if (!search)
+        return res.status(400).send({reason: "Missing query."})
+
+    const normalizedQuery = normalizeQuery(search);
+    const limit = (top && validLimit(Number(top))) ? top : DEFAULT_LIMIT;
+
+    try {
+        const results = lookupNamesDB(normalizedQuery, limit);
         return res.json(results);
     }
     catch (err) {
